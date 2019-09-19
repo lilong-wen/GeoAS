a7908237958554ed74efe60e71a2f2b026de705d
wenlilong <zndxwll@gmail.com>
2019-09-20 01:03:06 +0800

    add

--- data_generator.py
+++ data_generator.py
@@ -1,2 +1,121 @@
 # generate data using euklides
+import os
+import random
 
+vocabulary_Captal_list = ["A", "B", "C", "D", "E", "F", "G", "H", "I",
+    "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U",
+    "V", "W", "X", "Y", "Z"]
+
+vocabulary_No_Captal_list = ["a", "b", "c", "d", "e", "f", "g", "h", "i",
+    "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u",
+    "v", "w", "x", "y", "z"]
+
+triangle_type_list = ["triangle", "right", "isosceles", "equilateral"]
+
+triangle_type = random.choices(triangle_type_list, k=1)
+triangle_type = triangle_type[0].__str__()
+
+point_name_choice = random.choices(vocabulary_Captal_list, k=3)
+
+point_name_1 = point_name_choice[0].__str__()
+point_name_2 = point_name_choice[1].__str__()
+point_name_3 = point_name_choice[2].__str__()
+
+line_length_1 = 6
+line_length_1 = line_length_1.__str__()
+line_length_2 = 6
+line_length_2 = line_length_2.__str__()
+line_length_3 = 6
+line_length_3 = line_length_3.__str__()
+
+degree_less_than_90_list = ["30", "45", "60"]
+degree_list = ["30", "45", "60", "90", "120", "150", "180"]
+
+first_line_degree = random.choices(degree_list)[0]
+first_line_degree = first_line_degree.__str__()
+
+
+first_angle_degree = random.choices(degree_less_than_90_list)[0]
+first_angle_degree = first_angle_degree.__str__()
+
+second_angle_degree = random.choices(degree_less_than_90_list)[0]
+second_angle_degree = first_angle_degree.__str__()
+
+num = 0
+num = num.__str__()
+image_name = "img_" + num
+
+print(triangle_type)
+print(type(triangle_type))
+with open(image_name + '.euk', 'w') as tp:
+    tp.write("box -10, -10, 10, 10" + "\n")
+    tp.write(point_name_1 + " = point(0, 0)" + "\n")
+    if triangle_type == 'triangle':
+        tp.write(point_name_1 + " " 
+            + point_name_2 + " " 
+            + point_name_3 + " " 
+            + triangle_type + " " 
+            + line_length_1 + ", " 
+            + first_angle_degree + "deg" + ", "
+            + second_angle_degree + "deg" + ", "
+            + first_line_degree + "deg" + "\n")
+    
+    elif triangle_type == 'right':
+        tp.write(point_name_1 + " " 
+            + point_name_2 + " " 
+            + point_name_3 + " "
+            + triangle_type + " "
+            + line_length_1 + " "
+            + first_angle_degree + "deg" + ", "
+            + first_line_degree + "deg" + "\n")
+
+    elif triangle_type == 'isosceles':
+        tp.write(point_name_1 + " " 
+            + point_name_2 + " " 
+            + point_name_3 + " "
+            + triangle_type + " "
+            + line_length_1 + " "
+            + first_angle_degree + "deg" + ", "
+            + first_line_degree + "deg" + "\n")
+
+    elif triangle_type == 'equilateral':
+        tp.write(point_name_1 + " " 
+            + point_name_2 + " " 
+            + point_name_3 + " "
+            + triangle_type + " "
+            + line_length_1 + " "
+            + "\n")
+
+    tp.write("draw" + "\n")
+    tp.write("  " + "(" 
+        + point_name_1 + ". " 
+        + point_name_2 + ". " 
+        + point_name_3 + ")" + "\n")
+    tp.write("end" + "\n")
+
+    tp.write("label" + "\n")
+    tp.write('  ' + point_name_1 + ' ' + '180deg font("Consolas-Bold-12")' + "\n")
+    tp.write('  ' + point_name_2 + ' ' + '0deg font("Consolas-Bold-12")' + "\n")
+    tp.write('  ' + point_name_3 + ' ' + '90deg font("Consolas-Bold-12")' + "\n")
+    tp.write('end' + "\n")
+
+
+with open("lable.txt", 'a') as lb:
+    if triangle_type == "triangle":
+        lb.write("a " 
+            + triangle_type 
+            + " with points " 
+            + point_name_1 + " "
+            + point_name_2 + " "
+            + point_name_3 + " " 
+            + point_name_1 + point_name_2 + point_name_3 + "equal" + 
+            + "\n")
+    if triangle_type == "right":
+
+    if triangle_type == "isosceles":
+
+    if triangle_type == "equilateral":
+        
+
+cmd = "./convert.sh " + image_name + ".euk"
+os.system(cmd)
